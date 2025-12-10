549e0100cd788de15a0f2161625e3acd759b1786
tmalfi08 <t.m.alfi00@gmail.com>
2025-12-10 16:10:41 +0700

    Update Bus.java

--- Bus.java
+++ Bus.java
@@ -19,3 +19,49 @@ public class Bus {
         penumpangBerdiri = new ArrayList<>();
         totalPendapatan = 0;
     }
+
+    // Method Naikkan Penumpang
+    public boolean naikkanPenumpang(Penumpang p) {
+        // 1. Cek Saldo
+        if (p.getSaldo() < ONGKOS_BUS) {
+            System.out.println("Gagal: Saldo kurang! Saldo anda: " + p.getSaldo());
+            return false;
+        }
+
+        // 2. Cek Prioritas (Lansia > 60, Anak < 10, atau Hamil)
+        boolean isPrioritas = (p.getUmur() < 10 || p.getUmur() > 60 || p.getHamil());
+        boolean berhasilNaik = false;
+
+        if (isPrioritas) {
+            // Prioritas masuk ke: Kursi Prioritas -> Kursi Biasa -> Berdiri
+            if (penumpangPrioritas.size() < 4) {
+                penumpangPrioritas.add(p);
+                berhasilNaik = true;
+            } else if (penumpangBiasa.size() < 16) {
+                penumpangBiasa.add(p);
+                berhasilNaik = true;
+            } else if (penumpangBerdiri.size() < 20) {
+                penumpangBerdiri.add(p);
+                berhasilNaik = true;
+            }
+        } else {
+            // Penumpang Biasa masuk ke: Kursi Biasa -> Berdiri (TIDAK BOLEH KE PRIORITAS)
+            if (penumpangBiasa.size() < 16) {
+                penumpangBiasa.add(p);
+                berhasilNaik = true;
+            } else if (penumpangBerdiri.size() < 20) {
+                penumpangBerdiri.add(p);
+                berhasilNaik = true;
+            }
+        }
+
+        // Jika berhasil naik, potong saldo
+        if (berhasilNaik) {
+            p.kurangiSaldo(ONGKOS_BUS);
+            totalPendapatan += ONGKOS_BUS;
+            return true;
+        } else {
+            System.out.println("Gagal: Bus Penuh!");
+            return false;
+        }
+    }
