e331441dbb70a302638a96cc89a50f233d795415
tmalfi08 <t.m.alfi00@gmail.com>
2025-12-10 16:09:03 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -25,3 +25,55 @@ public class Main {
                 input.next(); // Buang input yang salah
                 continue;
             }
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Nama Penumpang: ");
+                    String nama = input.nextLine();
+
+                    System.out.print("Umur: ");
+                    int umur = 0;
+                    if (input.hasNextInt()) {
+                        umur = input.nextInt();
+                    } else {
+                        System.out.println("Umur harus angka! Dianggap 0.");
+                        input.next();
+                    }
+
+                    System.out.print("Apakah Hamil? (ketik 'y' jika ya,atau tidak): ");
+                    String hamilStr = input.next();
+                    boolean hamil = hamilStr.equalsIgnoreCase("y");
+
+                    Penumpang p = new Penumpang(nama, idCounter++, umur, hamil);
+                    
+                    if (transKoetaradja.naikkanPenumpang(p)) {
+                        System.out.println("-> BERHASIL: " + nama + " naik ke dalam bus.");
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("Masukkan nama penumpang yang turun: ");
+                    String namaTurun = input.nextLine();
+                    
+                    if (transKoetaradja.turunkanPenumpang(namaTurun)) {
+                        System.out.println("-> BERHASIL: " + namaTurun + " telah turun.");
+                    } else {
+                        System.out.println("-> GAGAL: Nama tidak ditemukan di dalam bus.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println(transKoetaradja.toString());
+                    break;
+
+                case 4:
+                    System.out.println("Program Selesai.");
+                    break;
+
+                default:
+                    System.out.println("Pilihan tidak valid.");
+            }
+        }
+        input.close();
+    }
+}
